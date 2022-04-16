206b1ad80ee251e79814a53d8e605468db52d4a3
mertoenjosh <mertoenjosh3527@gmail.com>
2022-04-16 21:18:44 +0300

    modified:   main.h

--- test/main.h
+++ test/main.h
@@ -4,7 +4,11 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 
 int _printf(const char *format, ...);
+void _vprintf(const char *format, va_list args);
+void num_to_str(uint64_t number, int base, char *buffer);
+void unsigned_num_to_str(uint64_t number, int base, char *buffer);
 
 #endif
