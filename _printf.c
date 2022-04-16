6f78d28a23b2b57f40389df3ca406beb8daf95f9
mertoenjosh <mertoenjosh3527@gmail.com>
2022-04-16 13:28:46 +0300

    modified:   _printf.c

--- _printf.c
+++ _printf.c
@@ -1,27 +1,95 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include "main.h"
 
-int main(void)
+/**
+ * print_int - prints integers
+ * @args: list of arguments
+ * @s: separator
+ */
+
+void print_int(va_list args, char *s)
 {
-	_printf("sj", "sksks", "gsgs");
-	return (0);
+	printf("%s%d", s, va_arg(args, int));
 }
 
-int _printf(const char *format, ...)
+/**
+ * print_float - prints floats
+ *
+ * @s: separator
+ * @args: arguments
+ */
+
+void print_float(va_list args, char *s)
 {
-	char **str;
-	int i = 0;
+	printf("%s%f", s, va_arg(args, double));
+}
+
+
+/**
+ * print_char - prints chars
+ *
+ * @args: arguments
+ * @s: separator
+ */
+
+void print_char(va_list args, char *s)
+{
+	printf("%s%c", s, va_arg(args, int));
+}
+
+/**
+ * print_str - prints strings
+ *
+ * @args: arguments
+ * @s: separator
+ */
+
+void print_str(va_list args, char *s)
+{
+	char *str;
+
+	str = va_arg(args, char *);
+	if (str == NULL)
+		str = "(nil)";
+	printf("%s%s", s, str);
+}
+
+/**
+ * print_all - prints anything
+ *
+ * @format: a list of types of args passed
+ */
+
+void print_all(const char * const format, ...)
+{
 	va_list args;
+	int i, j;
+	char *s;
+
+	type_t ops[] = {
+		{"i", print_int},
+		{"c", print_char},
+		{"f", print_float},
+		{"s", print_str},
+		{NULL, NULL}
+	};
 
 	va_start(args, format);
-	
-	while (format[i])
+	i = 0;
+	s = "";
+	while (format != NULL && format[i] != '\0')
 	{
-		str = va_arg(args, char**);
-		printf("%s ", str);
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == *(ops[j]).op)
+			{
+				ops[j].f(args, s);
+				s = ", ";
+			}
+			j++;
+		}
 		i++;
 	}
+	printf("\n");
 	va_end(args);
-	return (0);
 }
