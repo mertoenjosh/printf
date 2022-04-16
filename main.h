78640a196bfa4087c287a961577c207b8ce3cee9
mertoenjosh <mertoenjosh3527@gmail.com>
2022-04-16 13:29:01 +0300

    modified:   main.h

--- main.h
+++ main.h
@@ -1,6 +1,31 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
-int _printf(const char *format, ...);
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-#endif __MAIN_H__
+#ifndef __STRUCTS_H__
+#define __STRUCTS_H__
+/**
+ * struct type - data type
+ *
+ * @op: data type argument
+ * @f: function
+ */
+
+typedef struct type
+{
+	char *op;
+	void (*f)(va_list, char *);
+} type_t;
+
+#endif
+
+int _printf(const char *format, ...);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
