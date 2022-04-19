e85a3e224a5deb1bc1907735d5afd61bd5fef981
karey njeri <kareynjeri@gmail.com>
2022-04-19 11:47:20 +0300

    modified:   _vprintf.c

--- _vprintf.c
+++ _vprintf.c
@@ -1,4 +1,12 @@
 #include "main.h"
+/**
+*_vprintf-prints out the arguements 
+* print_string-prints strings
+* print_int-printes intergers*format
+*@format:lists the number of arguements in function prototype
+*@args:arguements found in function
+*Return:returns the number of character printed
+*/
 
 int _vprintf(const char *format, va_list args)
 {
